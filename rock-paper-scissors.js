01af5108ab651baa129b3f97f437660304928f29
ragunath-br <153410999+ragunath-br@users.noreply.github.com>
2024-02-02 20:22:40 +0530

    CREATES FUNCTION PLAYROUND WHICH GENERATES RESULTS
    This will give the results based on two choices given to the function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -31,5 +31,51 @@ function formatTextForConsole(text){
 //Function that plays 1 round of the game Rock-Paper-Scissors
 //Returns the result of the game
 function playRound(playerSelection,computerSelection){
+    let result = '';
+    if (playerSelection === 'rock'){
+        if (computerSelection === 'rock'){
+            result = `You are tied. ${formatTextForConsole(playerSelection)} ties ${formatTextForConsole(computerSelection)}.`;
+        }
+        else if(computerSelection === 'paper'){
+            result = `You Lose. ${formatTextForConsole(computerSelection)} beats ${formatTextForConsole(playerSelection)}.`;
+        }
+        else{
+            result = `You Win. ${formatTextForConsole(playerSelection)} beats ${formatTextForConsole(computerSelection)}.`; 
+        }
+    }
     
+    else if (playerSelection === 'paper'){
+        if (computerSelection === 'rock'){
+            result = `You Win. ${formatTextForConsole(playerSelection)} beats ${formatTextForConsole(computerSelection)}.`;
+        }
+        else if(computerSelection === 'paper'){
+            result = `You are tied. ${formatTextForConsole(playerSelection)} ties ${formatTextForConsole(computerSelection)}.`;
+        }
+        else{
+            result = `You Lose. ${formatTextForConsole(computerSelection)} beats ${formatTextForConsole(playerSelection)}.`; 
+        }
+    }
+
+    else{
+        if (computerSelection === 'rock'){
+            result = `You Lose. ${formatTextForConsole(computerSelection)} beats ${formatTextForConsole(playerSelection)}.`;
+        }
+        else if(computerSelection === 'paper'){
+            result = `You Win. ${formatTextForConsole(playerSelection)} beats ${formatTextForConsole(computerSelection)}.`;
+        }
+        else{
+            result = `You are tied. ${formatTextForConsole(playerSelection)} ties ${formatTextForConsole(computerSelection)}.`; 
+        }
+    }
+    return result;
+}
+
+//const playerSelection = "rock";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
+
+function playGame(){
+    for(let i = 0; i < 5;i++){
+        
+    }
 }
