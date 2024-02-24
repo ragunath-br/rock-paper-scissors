d3f4851aec2e8db42fa6daa3f3ddaefd54146287
ragunath-br <153410999+ragunath-br@users.noreply.github.com>
2024-02-24 12:36:41 +0530

    MODIFIES CODE TO FIT REQ#3
    Ensures that all info is displayed in the web page rather than the console log to make the webpage more interactive. Creates and adds required elements to be displayed in the web page.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,23 @@
+
+let playerScore = 0;
+let computerScore = 0;
+
+//Functions to add listeners and call function when user clicks button
+//Rather than adding eventlisteners to each button, it is better to add event listener to the parent of the buttons
+const buttonMenu = document.querySelector('#buttonMenu');
+buttonMenu.addEventListener('click',playGame);
+
+//Adding element to identify the div for showing result page
+const resultPage = document.querySelector('#resultPage');
+
+//Setting up the elements earlier so that updating them is possible rather than appending over and over
+const roundResultMessage = document.createElement('p');
+resultPage.appendChild(roundResultMessage);
+
+//Setting up the final result element in the page below round result
+const finalResultMessage = document.createElement('h3');
+resultPage.appendChild(finalResultMessage);
+
 //Function to get the computer choice
 //Returns one of three choices - Rock/Paper/Scissors
 function getComputerChoice(){
@@ -8,25 +28,17 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-//console.log(getComputerChoice());
-
 //Function for formatting text
 //Takes text ip and converts into lowercase
 function formatTextForCode(text){
     return text.toLowerCase();
 }
 
-//console.log(formatTextForCode('Gryat'))
-
-
 //Function for formatting text
 //Takes text ip and converts into firstletter Capital , rest small case for displaying in console.
-function formatTextForConsole(text){
+function formatTextForWebPage(text){
     return text[0].toUpperCase() + (text.slice(1).toLowerCase());
 }
-
-//console.log(formatTextForConsole('gryat'))
-
 
 //Function that plays 1 round of the game Rock-Paper-Scissors
 //Returns the result of the game
@@ -70,44 +82,31 @@ function playRound(playerSelection,computerSelection){
     return result;
 }
 
-//const playerSelection = "rock";
-//const computerSelection = getComputerChoice();
-//console.log(playRound(playerSelection, computerSelection));
+//Function that shows the person who reaches 5 victories first as the winner
 
-//Functions to add listeners and call function when user clicks button
-//Rather than adding eventlisteners to each button, it is better to add event listener to the parent of the buttons
-const buttonMenu = document.querySelector('#buttonMenu');
-buttonMenu.addEventListener('click',playGame);
+function playGame(event){    
+    let playerSelection = event.target.id;
+    let computerSelection = getComputerChoice();
+    let roundResult = playRound(playerSelection,computerSelection);        
+    let gameResult = '';
 
+    if(roundResult === 'win'){
+        playerScore+=1;
+        gameResult = `You Win. ${formatTextForWebPage(playerSelection)} beats ${formatTextForWebPage(computerSelection)}.`;
+    }
+    else if(roundResult === 'loss'){
+        computerScore+=1;
+        gameResult = `You Lose. ${formatTextForWebPage(computerSelection)} beats ${formatTextForWebPage(playerSelection)}.`;
+    }else{
+        gameResult = `It\'s a Tie. ${formatTextForWebPage(playerSelection)} ties with ${formatTextForWebPage(computerSelection)}.`; 
+    }
+    
+    roundResultMessage.innerText = gameResult;
+        
 
-
-//Function that plays 5 rounds of the game Rock-Paper-Scissors while keeping track of score
-//Returns the winner of the 5 game series
-
-function playGame(event){
-    let playerScore = 0;
-    let computerScore = 0;
-    //for(let i = 0; i < 5;i++){
-        //let playerSelection = formatTextForCode(prompt("Enter your Choice:"))
-        let playerSelection = event.target.id;
-        let computerSelection = getComputerChoice();
-        let roundResult = playRound(playerSelection,computerSelection);        
-        let gameResult = '';
-        //console.log(roundResult);
-
-        if(roundResult === 'win'){
-            playerScore+=1;
-            gameResult = `You Win. ${formatTextForConsole(playerSelection)} beats ${formatTextForConsole(computerSelection)}.`;
-        }
-        else if(roundResult === 'loss'){
-            computerScore+=1;
-            gameResult = `You Lose. ${formatTextForConsole(computerSelection)} beats ${formatTextForConsole(playerSelection)}.`;
-        }else{
-            gameResult = `It\'s a Tie. ${formatTextForConsole(playerSelection)} ties with ${formatTextForConsole(computerSelection)}.`; 
-        }
-        console.log(gameResult);
-    //}
-    console.log(finalResult(playerScore,computerScore));
+    if((playerScore===5) || (computerScore == 5)){
+        finalResultMessage.innerText = finalResult(playerScore,computerScore);
+    }    
 }
 
 
@@ -118,7 +117,7 @@ function finalResult(playerScore,computerScore){
         resultStr+='You Win. Congrats!!';
     }
     else if(computerScore > playerScore){
-        resultStr+='You Lose. The rise of the machine begins!!'; 
+        resultStr+='You Lose. The rise of the machines has begun!!'; 
     }
     else{
         resultStr+='It\'s a tie. You didn\'t win but you didn\'t lose either.';
